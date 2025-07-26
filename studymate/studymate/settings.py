712abb3afa7b79b3e089bfd0f48aa3c1f2bf7890
JosephB <jsphbnda@gmail.com>
2025-07-26 21:33:14 +0200

    progress report

--- studymate/studymate/settings.py
+++ studymate/studymate/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# URL for referencing static files
 STATIC_URL = '/static/'
 
-# Directory for storing static files (useful for development)
 STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # For collectstatic in production
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
